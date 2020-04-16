6941b3eb5cca81fe8717f4111dac92cb42b9244f
Takaf19 <tk.f19ms@gmail.com>
2020-04-17 03:46:41 +0900

    delete file

--- db/migrate/20200403132915_create_explanations.rb
+++
@@ -1,9 +0,0 @@
-class CreateExplanations < ActiveRecord::Migration[5.2]
-  def change
-    create_table :explanations do |t|
-      t.text        :text,       null: false
-      t.references  :recreation, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
