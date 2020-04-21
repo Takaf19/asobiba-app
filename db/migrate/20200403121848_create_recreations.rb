1b83cf1dcbaac388fe38a193fbb9c09e60421cac
Takaf19 <tk.f19ms@gmail.com>
2020-04-22 00:37:42 +0900

    Update　migration

--- db/migrate/20200403121848_create_recreations.rb
+++ db/migrate/20200403121848_create_recreations.rb
@@ -3,6 +3,7 @@ class CreateRecreations < ActiveRecord::Migration[5.2]
     create_table :recreations do |t|
       t.string      :recname,              null: false
       t.text        :recimage,             null: false
+      t.integer     :recimageType,         null: false
       t.text        :recComment,           null: false
       t.integer     :rectime_id,           null: false       
       t.integer     :requirednumber_id,    null: false
