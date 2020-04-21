01f38a20ac1696267dc8297de258c9d1ab845541
Takaf19 <tk.f19ms@gmail.com>
2020-04-22 00:23:40 +0900

    recreations_controllerの修正
    imageテーブルを保存できるように修正

--- app/controllers/recreations_controller.rb
+++ app/controllers/recreations_controller.rb
@@ -14,12 +14,14 @@ class RecreationsController < ApplicationController
 
   def new
     @recreation = Recreation.new
-    @recreation.explanations.build
+    explanation = @recreation.explanations.build
+    explanation.build_image
   end
 
   def create
     # 親要素を保存かけてあげることで自動で子要素も保存される！
     recreation = Recreation.new(rec_params)
+    recreation.recimageType = imageType(recreation)
     recreation.save
     redirect_to root_path
   end
@@ -27,7 +29,15 @@ class RecreationsController < ApplicationController
   private 
   def rec_params
       # 子要素のstrong parameterを書くことで、自動で子要素に親要素のidもふられる。explanatinテーブルにあるrecreation_idに親要素のidがふられます。
-      params.require(:recreation).permit(:recname, :recimage, :recComment, :requirednumber_id, :rectime_id, explanations_attributes: [:recText]).merge(user_id: current_user.id)
+      params.require(:recreation).permit(:recname, :recimage, :recComment, :requirednumber_id, :rectime_id, explanations_attributes: [:recText, :_destroy, image_attributes: [:imgurl, :_destroy]]).merge(user_id: current_user.id)
+  end
+
+  def imageType(target)
+    if target.recimage.content_type.include?("image/")
+      return "画像"
+    else
+      return "動画"
+    end
   end
 
 end
