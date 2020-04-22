dbcb77425b1bb379d1468c87c322b86d4bc49289
Takaf19 <tk.f19ms@gmail.com>
2020-04-22 15:10:04 +0900

    Update migration

--- db/migrate/20200503132925_create_images.rb
+++ db/migrate/20200503132925_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
       t.text        :imgurl,           null: false
-      t.references  :explanation,      foreign_key: true
+      t.references  :explanation,      null: false, foreign_key: true
       t.timestamps
     end
   end
