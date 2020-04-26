09b4ec42f890fc371a6b5526d0012db78c3364bd
Takaf19 <tk.f19ms@gmail.com>
2020-04-27 00:58:16 +0900

    create vueFile

---
+++ app/javascript/myPage/profile.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="profile">
+    <div class="profile__image">
+      <img src="/assets/NoImage.png" class="image" />
+      <p>名前</p>
+      <div class="profile__image--items">
+      </div>
+    </div>
+    <div class="user-counts">
+      <span>
+        10
+        <div class="icon">
+          <i class="fas fa-pencil-alt"></i>
+          <span>投稿した数</span>
+        </div>
+      </span>
+      <span>
+        10
+        <div class="icon">
+          <i class="fas fa-star"></i>
+          <span>お気に入りした数</span>
+        </div>
+      </span>
+    </div>
+    <div class="profile__contents">
+      <div class="user-info">
+        <div class="profile__contents--item profile__contents--name">
+          <p>Name</p>
+          <span class="sankaku">▶︎</span>
+          <span class="user-item">なまえ</span>
+        </div>
+        <div class="profile__contents--item profile__contents--mail">
+          <p>Mail</p>
+          <span class="sankaku">▶︎</span>
+          <span class="user-item">mail</span>
+        </div>
+        <div class="profile__contents--editPass">
+          <div class="editPass-text">
+            <p>Passwordの編集</p>
+          </div>
+        </div>
+      </div>
+      <div class="profile__contents--edit">
+        <div class="edit-text">
+          <p>情報の編集</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ app/javascript/userShow.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="myPageContents">
+    <div class="side-bar">
+      まだだよ！
+    </div>
+    <div id="myPage-Pages">
+      <profile />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Profile from './myPage/profile.vue'
+export default {
+  components: {
+    'profile': Profile,
+  }
+}
+</script>
