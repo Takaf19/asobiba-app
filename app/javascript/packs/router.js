bea09e6c98be8550a23e3f0812396b91f33bd739
Takaf19 <tk.f19ms@gmail.com>
2020-04-27 00:58:27 +0900

    Update router.js

--- app/javascript/packs/router.js
+++ app/javascript/packs/router.js
@@ -4,16 +4,11 @@ import newRec from '../newRec.vue'
 
 Vue.use(VueRouter)
 
+const routes = [
+  { path: '/', component: newRec },
+]
+
 export default new VueRouter({
   mode: 'history',
-  routes: [
-    { path: '/', component: newRec },
-  ],
-})
-
-export let tabs = new VueRouter({
-  mode: 'history',
-  routes: [
-    { path: '/newReceations', name:"newReceations", component: newRec },
-  ],
+  routes: routes,
 })
