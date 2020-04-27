1ede0bdc9b327ae44f8d4ed647729688272542e7
Takaf19 <tk.f19ms@gmail.com>
2020-04-27 23:10:43 +0900

    Update view

--- app/javascript/myPage/profile.vue
+++ app/javascript/myPage/profile.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="profile">
     <div class="profile__image">
-      <img src="/assets/NoImage.png" class="image" />
-      <p>名前</p>
+      <img v-if="userImage(user.user_image)" src="/assets/NoImage.png" class="image" />
+      <img v-else :src="rec.recimage.url" class="image" />
+      <p>{{ user.nickname }}</p>
       <div class="profile__image--items">
       </div>
     </div>
     <div class="user-counts">
       <span>
-        10
+        {{ recreations.length }}
         <div class="icon">
           <i class="fas fa-pencil-alt"></i>
           <span>投稿した数</span>
         </div>
       </span>
       <span>
-        10
+        {{ bookmarks.length }}
         <div class="icon">
           <i class="fas fa-star"></i>
           <span>お気に入りした数</span>
@@ -27,24 +28,41 @@
         <div class="profile__contents--item profile__contents--name">
           <p>Name</p>
           <span class="sankaku">▶︎</span>
-          <span class="user-item">なまえ</span>
+          <span class="user-item">{{ user.nickname }}</span>
         </div>
         <div class="profile__contents--item profile__contents--mail">
           <p>Mail</p>
           <span class="sankaku">▶︎</span>
-          <span class="user-item">mail</span>
+          <span class="user-item">{{ user.email }}</span>
         </div>
-        <div class="profile__contents--editPass">
+        <a href="#" class="profile__contents--editPass">
           <div class="editPass-text">
             <p>Passwordの編集</p>
           </div>
-        </div>
+        </a>
       </div>
       <div class="profile__contents--edit">
-        <div class="edit-text">
+        <a href="#" class="edit-text">
           <p>情報の編集</p>
-        </div>
+        </a>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+  import axios from "axios";
+  export default {
+    props: ['user', 'recreations', 'bookmarks'],
+    methods: {
+      userImage(target) {
+        if (target == null) {
+          return true
+        } else {
+          return false
+        }
+      },
+    },
+  };
+
+</script>
--- app/javascript/userShow.vue
+++ app/javascript/userShow.vue
@@ -1,19 +1,77 @@
 <template>
   <div id="myPageContents">
     <div class="side-bar">
-      まだだよ！
+      <div class="side-ber--link" v-bind:class="{ 'active': page0 }" @click="current=0" >
+        <p>ユーザー情報</p>
+      </div>
+      <div class="side-ber--link" v-bind:class="{ 'active': page1 }" @click="current=1" >
+        <p>お気に入り一覧</p>
+      </div>
+      <div class="side-ber--link" v-bind:class="{ 'active': page2 }" @click="current=2" >
+        <p>投稿一覧</p>
+      </div>
     </div>
     <div id="myPage-Pages">
-      <profile />
+      <!-- <profile /> -->
+      <div v-bind:is="component" :user="user" :recreations="recreations" :bookmarks="bookmarks"></div>
     </div>
   </div>
 </template>
 
 <script>
-  import Profile from './myPage/profile.vue'
+import Profile from "./myPage/profile.vue";
+import LikePage from "./myPage/likePage.vue";
+import UserRec from "./myPage/userRec.vue";
+import axios from "axios";
 export default {
   components: {
-    'profile': Profile,
-  }
-}
+    profile: Profile,
+    likePage: LikePage,
+    userRec: UserRec,
+  },
+  data: function() {
+    return {
+    componentTypes: ["profile", "likePage","userRec"],
+    current: 0,
+    i : 0,
+    page0: true,
+    page1: false,
+    page2: false,
+    user: Object,
+    recreations:[],
+    bookmarks: [],
+    };
+  },
+  computed: {
+    component: function() {
+      // 一致しているコンポーネント名を返す
+      return this.componentTypes[this.current];
+    }
+  },
+    created: function() {
+      this.userInfo();
+    },
+    methods: {
+      userInfo() {
+        axios.get(`/api/users/${this.$route.params['id']}`).then(
+          res => {
+            // ユーザー情報
+            this.user = res.data.user
+            // 投稿情報
+            for (var i = 0; i < res.data.recreations.length; i++) {
+              this.recreations.push(res.data.recreations[i]);
+            }
+            // ブックマーク
+            for (var i = 0; i < res.data.bookmarks.length; i++) {
+              this.bookmarks.push(res.data.bookmarks[i]);
+            }
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      },
+    },
+};
+
 </script>
