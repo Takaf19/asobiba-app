8d5e7784793938381bd456dbb9c89b28410efeb0
Takaf19 <tk.f19ms@gmail.com>
2020-05-07 12:11:44 +0900

    Update router

--- app/javascript/packs/router.js
+++ app/javascript/packs/router.js
@@ -2,11 +2,14 @@ import Vue from 'vue/dist/vue.esm.js'
 import VueRouter from 'vue-router'
 import newRec from '../newRec.vue'
 import UserShow from '../userShow.vue'
+import AllNewRec from '../topPage/allRecShow.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', component: newRec },
+  { path: '/recreations/allNewRecreasions', name: "allNewRec", component: AllNewRec },
+  // { path: '/allPopularRecreasions', component: AllRecShow },
   { path: '/users/:id', component: UserShow },
 ]
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,30 @@ Rails.application.routes.draw do
   devise_for :users
   root 'recreations#index'
 
+  # 呼び出し元のページを見にいくよう設定することでルーティングエラーを回避
+  get 'recreations/allNewRecreasions', to: 'recreations#index'
+  get 'recreations/allPopularRecreasions', to: 'recreations#index'
+
   resources :users, only: :show do
     resources :recreations, only: [ :new ]
   end
 
   resources :recreations, only: [ :index, :show, :new, :create ] do
     resource :bookmarks, only:[:create, :destroy]
+    collection do
+      get :allNewRecreasions
+      get :allPopularRecreasions
+    end
   end
   
   resources :images, only: [ :create ]
 
   namespace :api do
-    resources :recreations, only:[:index, :new, :create]
+    resources :recreations, only:[:index, :new, :create] do
+      collection do
+        get :allNewRec
+      end
+    end
     resources :users, only:[:show]
   end
 end
