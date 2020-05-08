7684db0c207ae8d745005c33032e3e9982a4213c
Takaf19 <tk.f19ms@gmail.com>
2020-05-08 14:20:06 +0900

    update controller

--- app/controllers/api/recreations_controller.rb
+++ app/controllers/api/recreations_controller.rb
@@ -3,13 +3,13 @@ class Api::RecreationsController < ApplicationController
   def index
     @recreations = Recreation.includes(:bookmarks).all.order(updated_at: "DESC").limit(10)
     # ブックマーク数上位１０件の記事を降順に並び替え
-    @popular = Recreation.find(Bookmark.group(:recreation_id).order('count(recreation_id) desc', updated_at: "DESC").limit(10).pluck(:recreation_id))
+    @popular = Recreation.find(Bookmark.group(:recreation_id).order("count(recreation_id) desc, updated_at DESC").limit(10).pluck(:recreation_id))
     render 'index', formats: 'json', handlers: 'jbuilder'
   end
 
   def allNewRec
     @recreations = Recreation.includes(:bookmarks).all.order(updated_at: "DESC")
-    @populars = Recreation.find(Bookmark.group(:recreation_id).order('count(recreation_id) desc', updated_at: "DESC").pluck(:recreation_id))
+    @populars = Recreation.find(Bookmark.group(:recreation_id).order("count(recreation_id) desc, updated_at DESC").pluck(:recreation_id))
     render 'allNewRec', formats: 'json', handlers: 'jbuilder'
   end
 
